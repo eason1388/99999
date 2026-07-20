b7ebe43f5e0b6c47ae1232ff2befc042855224da
eason1388 <jackkok1388@gmail.com>
2026-07-21 00:06:25 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 離線快取:安裝時快取主檔+各功能模組,之後網路優先、失敗回退快取(確保更新即時、離線可用)
-const CACHE='fsc-v8';
-const CORE=['./index.html','./manifest.json','./icon-192.png','./icon-512.png',
+const CACHE='fsc-v9';
+const CORE=['./index.html','./manifest.json','./icon-192.png','./icon-512.png','./場所佈局.html',
  './羅盤互動原型.html','./飛星八宅互動原型.html','./擇時飛星互動原型.html','./八字整合互動原型.html',
  './地圖測向互動原型.html','./尋龍模式互動原型.html','./教學中心互動原型.html','./風水分析報告原型.html',
  './平面圖疊盤.html','./勘察流程.html','./使用說明.html'].map(u=>encodeURI(u));
