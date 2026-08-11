b75bee7485f522ffe2ea651532cf4b9c9fbcf568
eason1388 <jackkok1388@gmail.com>
2026-08-11 15:22:14 +0800

    Update pack.js

--- pack.js
+++ pack.js
@@ -13,7 +13,7 @@
    ───────────────────────────────────────────── */
 (function(g){
   'use strict';
-  var CACHE_KEY = 'bpsy_pack_v3';
+  var CACHE_KEY = 'bpsy_pack_v4';
   var BIN_URL   = 'pack.bin';
   var mem = null;      // 解開後的判語物件
   var pending = null;  // 進行中的載入 Promise
